7b4bbb039aa71fbc0502b0182c62fe0cd99c17e8
Ayokayzy <theayokayzy1@gmail.com>
2024-04-05 05:32:31 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,16 @@
 #!/usr/bin/env bash
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-PACKAGE_NAME="nginx"
-FOLDER_PATH="/data/web_static/releases/test/"
-FOLDER_PATH2="/data/web_static/shared/"
-CONFIG_STR="server {
-        listen 80 default_server;
-        # server_name _;
-	index index.html
-        location /hbnb_static {
-                alias /data/web_static/current/;
-        }
-}"
-DESTINATION="/etc/nginx/sites-enabled/airbnb.conf"
-DESTINATION2="/data/web_static/current"
-
-# Check if the package is installed
-if dpkg -s $PACKAGE_NAME &> /dev/null; then
-	echo "$PACKAGE_NAME is already installed."
-else
-	# Install the package
-	echo "Installing $PACKAGE_NAME..."
-	#apt install nginx -y
-fi
-# Check if the folder exists
-if [ ! -d "$FOLDER_PATH" ]; then
-	# If the folder does not exist, create it
-	echo "Creating folder: $FOLDER_PATH"
-	mkdir -p "$FOLDER_PATH"
-fi
-if [ ! -d "$FOLDER_PATH2" ]; then
-	# If the folder does not exist, create it
-	echo "Creating folder: $FOLDER_PATH2"
-	mkdir -p "$FOLDER_PATH2"
-fi
-# touch /data/web_static/releases/test/index.html
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -42,18 +18,10 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-if [ -L "$DESTINATION2" ]; then
-    echo "Symbolic link already exists at $DESTINATION. Deleting..."
-    sudo rm "$DESTINATION2"
-fi
-ln -s "$FOLDER_PATH" /Data/web_static/current
-chown -R ubuntu:ubuntu /data/
-#add configuration text to /etc/nginx/sites-available/airbnb.conf
-echo "$CONFIG_STR" | tee /etc/nginx/sites-available/airbnb.conf
-# Check if the destination link already exists
-if [ -L "$DESTINATION" ]; then
-    echo "Symbolic link already exists at $DESTINATION. Deleting..."
-    rm "$DESTINATION"
-fi
-sudo ln -s /etc/nginx/sites-available/airbnb.conf /etc/nginx/sites-enabled/
-service nginx restart
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
