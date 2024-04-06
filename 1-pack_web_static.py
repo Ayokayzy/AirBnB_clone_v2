dfa4ef4f7f718b6fcaa06cd7aeb04307646be352
Ayokayzy <theayokayzy1@gmail.com>
2024-04-06 17:48:03 +0100

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-1-pack_web_static.py
-"""
-from fabric import Connection, task
-from datetime import datetime
-import os
+from fabric.api import local
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """a Fabric script that generates a .tgz archive \
-from the contents of the web_static folder"""
+    """ A script that generates archive the contents of web_static folder"""
 
-    FORMAT = '%Y%m%d%H%M%S'
-    TIME = datetime.now().strftime(FORMAT)
-    FILE_NAME = f"web_static_{TIME}.tgz"
-    c = Connection(
-        host="34.234.201.107",
-        user="ubuntu",
-        connect_kwargs={
-            "key_filename": "/home/ayokayzy/.ssh/id_rsa",
-        },
-    )
-    HOST = c.host
-    print(f"Packing web_static to versions/{FILE_NAME}")
-    print(f"[localhost] local: tar -czvf versions/{FILE_NAME} web_static/*")
-    result = c.local("mkdir -p versions")
-    if (result.ok):
-        c.local(f"tar -czvf versions/{FILE_NAME} compress_file/* 2>/dev/null")
-        file_size = os.path.getsize(f"versions/{FILE_NAME}")
-        print(f"web_static packed: versions/{FILE_NAME} -> {file_size}Bytes")
-    else:
-        print("None")
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
