7be21716b27486955db784dd3a5fedaeed2aa2ea
Ayokayzy <theayokayzy1@gmail.com>
2024-04-07 02:36:09 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,13 +17,22 @@ def do_deploy(archive_path):
         if not path.exists(archive_path):
             return False
 
-        get(archive_path, remote=f"/tmp/web_static_{filename}")
-        run(f"sudo mkdir -p /data/web_static/releases/")
-        run(f"sudo tar -xvzf web_static_{filename} -C \
-/data/web_static/releases/web_static_{filename}")
-        run(f"sudo rm -fr /tmp/web_static_{filename}")
-        run(f"sudo -fr rm /data/web_static/current")
-        run(f"sudo ln -s -f /data/web_static/releases/web_static_{filename} \
+        put(archive_path, remote=f"/tmp/")
+
+        timestamp = archive_path[-18:-4]
+        run(f"sudo mkdir -p /data/web_static/releases/web_static_{timestamp}")
+        run(f"sudo tar -xvzf web_static_{timestamp}.tgz -C \
+/data/web_static/releases/web_static_{timestamp}")
+        run(f"sudo rm -fr /tmp/web_static_{timestamp}.tgz")
+
+        # move contents into host web_static
+        run("sudo mv /data/web_static/releases/web_static_{timestamp}/web_static/* \
+/data/web_static/releases/web_static_{timestamp}/")
+        # remove extraneous web_static dir
+        run('sudo rm -rf /data/web_static/releases/\
+web_static_{timestamp}/web_static'
+        run(f"sudo rm -fr /data/web_static/current")
+        run(f"sudo ln -s -f /data/web_static/releases/web_static_{timestamp} \
 /data/web_static/current")
         return True
     except Exception:
