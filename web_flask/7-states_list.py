a22aab3afb1626e43d7fd0ffc2161b35c9895733
Ayokayzy <theayokayzy1@gmail.com>
2024-04-21 16:31:03 +0100

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+web_flask/7-states_list.py
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    states = storage.all(State)
+    all_state = {state.id: state.name for state in states.values()}
+    print(all_state);
+    return render_template("7-states_list.html", states=all_state)
+
+
+# Define a function to handle teardown
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
